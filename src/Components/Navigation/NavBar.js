5733ed498c37183a6f1b155e5889c9ad5caf988c
markgmz92 <markgmz92@gmail.com>
2021-01-29 21:02:28 +0800

    [PX70 - 004] Navbar style included

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import logo from "../../Assets/img/logo.png";
+import styles from "./NavBar.module.css";
+
 const NavBar = () => {
   return (
-    <nav>
-      <div>
+    <nav className={styles.nav}>
+      <div className={styles.logo}>
+        <img src={logo} alt="Proton Logo" />
+      </div>
+      <div className={styles.navLinks}>
         <Link to="#">
           <h2>Features</h2>
         </Link>
       </div>
-      <div>
+      <div className={styles.navLinks}>
         <Link to="#">
           <h2>Variant Comparison</h2>
         </Link>
       </div>
-      <div>
+      <div className={styles.navLinks}>
         <Link to="#">
           <h2>Price & Specification</h2>
         </Link>
